a65612b834049c5d3f2fb252124c81cdfa4479ce
Gregor Wolf <gregor.wolf@gmail.com>
2020-07-24 11:36:59 +0200

    Query ext. Service: filter,select,limit,orderBy

--- srv/service.js
+++ srv/service.js
@@ -1,10 +1,10 @@
 const cds = require('@sap/cds')
 /*
 const createFilter = xs => {
-	const andFilters = xs.map(x => new FilterList([
-		Suppliers.SUPPLIER.equals(x.businessPartner),
-	]))
-	return new FilterList(undefined, andFilters).flatten()
+  const andFilters = xs.map(x => new FilterList([
+    Suppliers.SUPPLIER.equals(x.businessPartner),
+  ]))
+  return new FilterList(undefined, andFilters).flatten()
 }
 
 function SELECT (columns) {
@@ -17,83 +17,83 @@ function SELECT (columns) {
 */
 
 function getJWT (req) {
-	if(typeof(req._) !== 'undefined') {
-		return retrieveJwt(req._.req)
-	} else {
-		return ""
-	}
+  if(typeof(req._) !== 'undefined') {
+    return retrieveJwt(req._.req)
+  } else {
+    return ""
+  }
 }
 
 module.exports = async function (){
 
-	const { Suppliers: sdkSuppliers } = require('./odata-client/EPM_REF_APPS_PROD_MAN_SRV')
-	const { FilterList, serializeEntity, retrieveJwt } = require('@sap-cloud-sdk/core')
-	
+  const { Suppliers: sdkSuppliers } = require('./odata-client/EPM_REF_APPS_PROD_MAN_SRV')
+  const { FilterList, serializeEntity, retrieveJwt } = require('@sap-cloud-sdk/core')
+  
   const externalService = await cds.connect.to('EPM_REF_APPS_PROD_MAN_SRV')
   const { Products, Suppliers } = externalService.entities
-	
-	const destinationName = 'ES5_SDK'
-	
-		
-	/*
-	this.before('READ', 'Orders', async (req) => {
+  
+  const destinationName = 'ES5_SDK'
+  
+    
+  /*
+  this.before('READ', 'Orders', async (req) => {
 
-		const { SELECT } = req.query
-		SELECT.columns = SELECT.columns.filter(c => !(c.expand && c.ref[0] === 'EPMBusinessPartner'))
+    const { SELECT } = req.query
+    SELECT.columns = SELECT.columns.filter(c => !(c.expand && c.ref[0] === 'EPMBusinessPartner'))
 
-	})
-	this.after('READ', 'Orders', async (results, req) => {
+  })
+  this.after('READ', 'Orders', async (results, req) => {
 
-		const { EPMBusinessPartners } = this.entities
+    const { EPMBusinessPartners } = this.entities
 
-		const $expand = req._.odataReq.getQueryOptions() && req._.odataReq.getQueryOptions().$expand || ''
-		const result = results[0] || {}
+    const $expand = req._.odataReq.getQueryOptions() && req._.odataReq.getQueryOptions().$expand || ''
+    const result = results[0] || {}
 
-		const entityRE = new RegExp(/([a-zA-Z]+)(?=(\(|$))/g)
+    const entityRE = new RegExp(/([a-zA-Z]+)(?=(\(|$))/g)
 
-		if ($expand){
-			if($expand.match(entityRE).includes('EPMBusinessPartner')) {
-				if('businessPartner' in result) {
-					var jwt = getJWT(req)
-					try{
-						debugger;
-						const epmSuppliers = await Suppliers
-						.requestBuilder()
-						.getAll()
-						.filter(createFilter(results))
-						.execute({ destinationName: destinationName, jwt: jwt})
-						.then(Suppliers => Suppliers.map(bp => serializeEntity(bp, Suppliers)))
+    if ($expand){
+      if($expand.match(entityRE).includes('EPMBusinessPartner')) {
+        if('businessPartner' in result) {
+          var jwt = getJWT(req)
+          try{
+            debugger;
+            const epmSuppliers = await Suppliers
+            .requestBuilder()
+            .getAll()
+            .filter(createFilter(results))
+            .execute({ destinationName: destinationName, jwt: jwt})
+            .then(Suppliers => Suppliers.map(bp => serializeEntity(bp, Suppliers)))
 
-						results.forEach(order => order.EPMBusinessPartner = SELECT (EPMBusinessPartners.elements) .from (epmSuppliers.find(
-							Suppliers => order.businessPartner === Suppliers.BpId
-						)))
-					} catch (e) {
-						debugger;
-						console.log("Error: " + e.message)
-						console.log("Stack: " + e.stack)
-					}
-				}
-			}
-		}
-	})
-	*/
-	this.on('READ', 'sdkSuppliers', async (results, req) => {
-		var jwt = getJWT(req)
-		try{
-			const epmSuppliers = await sdkSuppliers
-			.requestBuilder()
-			.getAll()
-			.execute({ destinationName: destinationName, jwt: jwt})
-			// console.log("epmSuppliers: " + JSON.stringify(epmSuppliers))
-			let mappedSuppliers = await epmSuppliers.map(bp => serializeEntity(bp, sdkSuppliers))
-			// console.log("mappedSuppliers: " + JSON.stringify(mappedSuppliers))
-			return mappedSuppliers
-		} catch (e) {
-			debugger;
-			console.error("Error: " + e.message)
-			console.log("Stack: " + e.stack)
-		}
-	})
+            results.forEach(order => order.EPMBusinessPartner = SELECT (EPMBusinessPartners.elements) .from (epmSuppliers.find(
+              Suppliers => order.businessPartner === Suppliers.BpId
+            )))
+          } catch (e) {
+            debugger;
+            console.log("Error: " + e.message)
+            console.log("Stack: " + e.stack)
+          }
+        }
+      }
+    }
+  })
+  */
+  this.on('READ', 'sdkSuppliers', async (results, req) => {
+    var jwt = getJWT(req)
+    try{
+      const epmSuppliers = await sdkSuppliers
+      .requestBuilder()
+      .getAll()
+      .execute({ destinationName: destinationName, jwt: jwt})
+      // console.log("epmSuppliers: " + JSON.stringify(epmSuppliers))
+      let mappedSuppliers = await epmSuppliers.map(bp => serializeEntity(bp, sdkSuppliers))
+      // console.log("mappedSuppliers: " + JSON.stringify(mappedSuppliers))
+      return mappedSuppliers
+    } catch (e) {
+      debugger;
+      console.error("Error: " + e.message)
+      console.log("Stack: " + e.stack)
+    }
+  })
 
   this.on ('READ','Suppliers', async req => {
     const tx = externalService.transaction(req)
@@ -107,15 +107,28 @@ module.exports = async function (){
     }
   })
 
-	this.on('READ', 'Products', async (results, req) => {
+  this.on('READ', 'Products', async (req) => {
     const tx = externalService.transaction(req)
-    const cqn = SELECT.from(Products)
+    var cqn = SELECT.from(Products)
+    if(req.query.SELECT.limit) {
+      cqn.SELECT.limit = req.query.SELECT.limit
+    }
+    if(req._.query && req._.query.$select) {
+      cqn.SELECT.columns = req.query.SELECT.columns
+    }
+    if(req._.query && req._.query.$filter) {
+      cqn.SELECT.where = req.query.SELECT.where
+    }
+    if(req.query.SELECT.orderBy) {
+      cqn.SELECT.orderBy = req.query.SELECT.orderBy
+    }
     try {
-			let result = await tx.run(cqn)
-			// console.log("Products: " + JSON.stringify(result))
+      let result = await tx.run(cqn)
       return result
     } catch (error) {
       console.error(error.message)
+      console.error(error.request._header)
+      req.error(error.message)
     }
-	})
+  })
 }
